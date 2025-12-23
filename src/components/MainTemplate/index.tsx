8fcf1de17b09192a95f375a4b3128e683ebc2653
Ryotruuser <ryonantruuser@gmail.com>
2025-12-23 11:49:56 -0300

    heading dinamico nas paginas

--- src/components/MainTemplate/index.tsx
+++ src/components/MainTemplate/index.tsx
@@ -1,16 +1,43 @@
 import { Link } from "react-router-dom";
 import BodyApp from "../BodyApp/BodyApp";
 import Heading from "../Heading/Heading";
+import { ArrowLeft } from "lucide-react";
 
 type MainTemplateProps = {
   children: React.ReactNode;
 };
 
 export function MainTemplate({ children }: MainTemplateProps) {
+  const home = "/";
+  const url = window.location.href;
+
+  function isThisHome() {
+    if (
+      url.includes("Calc") ||
+      url.includes("Cambio") ||
+      url.includes("Tasks") ||
+      url.includes("Timer")
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  const styleOutHome = {
+    fontSize: 32,
+  };
+
   return (
     <>
       <Heading>
-        <Link to={"/"}>multitask app</Link>
+        {isThisHome() ? (
+          <Link to={home}>multitask app</Link>
+        ) : (
+          <Link to={home} style={styleOutHome}>
+            <ArrowLeft /> Voltar a home
+          </Link>
+        )}
       </Heading>
 
       <BodyApp>{children}</BodyApp>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { MainTemplate } from "../../components/MainTemplate";
+import styles from "./styles.module.css";
 
 export function Home() {
   const pages = ["Calc", "Cambio", "Tasks", "Timer"];
@@ -8,7 +9,7 @@ export function Home() {
     <>
       <MainTemplate>
         {pages.map((item, index) => (
-          <Link key={index} to={`/${item}`}>
+          <Link key={index} to={`/${item}`} className={styles.imgApp}>
             <img src={`./${item}.png`} alt={`icone do aplicativo ${item}`} />
           </Link>
         ))}
