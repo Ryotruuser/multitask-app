f301670f033e56c275c64778103134761e02622b
Ryotruuser <ryonantruuser@gmail.com>
2025-12-25 10:43:51 -0300

    temporizador ok conceitualmente acredito

--- src/pages/Timer/index.tsx
+++ src/pages/Timer/index.tsx
@@ -6,26 +6,68 @@ import { Play, Square } from "lucide-react";
 
 export function Timer() {
   interface timer {
-    tempoTotal: number;
-    tempoAtual: number;
+    allTimeInSeconds: number;
+    actualTimeInSeconds: number;
     isPaused: boolean;
   }
+
+  interface selectedTimer {
+    hoursSelected: number;
+    minutesSelected: number;
+    secondsSelected: number;
+  }
+
   const [timer, setTimer] = useState<timer>();
+  const [selectedTimer, setSelectedTimer] = useState<selectedTimer>();
+
+  function doIhaveTime() {
+    const hours = selectedTimer?.hoursSelected;
+    const minutes = selectedTimer?.minutesSelected;
+    const seconds = selectedTimer?.secondsSelected;
+    if (hours || minutes || seconds) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function putAllMyTimeTogether() {
+    const doWeHaveAnyTime = doIhaveTime();
+    if (doWeHaveAnyTime && selectedTimer) {
+      const everyInputValue =
+        selectedTimer.hoursSelected * 3600 +
+        selectedTimer.minutesSelected * 60 +
+        selectedTimer.secondsSelected;
+
+      return everyInputValue;
+    } else {
+      return 0;
+    }
+  }
 
   function handleTimer() {
+    const allTimeInSeconds = putAllMyTimeTogether();
+    console.log(typeof allTimeInSeconds);
+    setTimer({
+      allTimeInSeconds: allTimeInSeconds,
+      actualTimeInSeconds: 0,
+      isPaused: false,
+    });
+
     console.log(timer);
   }
 
   function formatTimer(timerNameType: string) {
-    if (timer?.tempoTotal) {
+    const doWeHaveAnyTime = doIhaveTime();
+    if (doWeHaveAnyTime) {
       if (timerNameType == "hours") {
-        const newHours = Math.floor(timer?.tempoTotal / 3600);
+        const newHours = selectedTimer?.hoursSelected;
         return String(newHours).padStart(2, "0");
       } else if (timerNameType == "minutes") {
-        const newMinutes = Math.floor((timer?.tempoTotal % 3600) / 60);
+        const newMinutes = selectedTimer?.minutesSelected;
         return String(newMinutes).padStart(2, "0");
       } else if (timerNameType == "seconds") {
-        const newSeconds = Math.floor(timer?.tempoTotal % 60);
+        const newSeconds = selectedTimer?.secondsSelected;
         return String(newSeconds).padStart(2, "0");
       }
     }
@@ -45,20 +87,71 @@ export function Timer() {
         </section>
 
         <section className="setTimerValues">
-          <input
-            type="range"
-            className="setTimerHours"
-            min={0}
-            max={720}
-            step={0.1}
-            onChange={(e) =>
-              setTimer({
-                tempoTotal: parseFloat(e.target.value) * 60,
-                tempoAtual: parseFloat(e.target.value) * 60,
-                isPaused: false,
-              })
-            }
-          />
+          <div className="inputTimerSet">
+            <h4>HH:</h4>
+            <input
+              type="range"
+              className="setTimerHours"
+              min={0}
+              max={11}
+              step={1}
+              onChange={(e) => {
+                setSelectedTimer({
+                  hoursSelected: parseFloat(e.target.value),
+                  minutesSelected: selectedTimer?.minutesSelected
+                    ? selectedTimer.minutesSelected
+                    : 0,
+                  secondsSelected: selectedTimer?.secondsSelected
+                    ? selectedTimer.secondsSelected
+                    : 0,
+                });
+              }}
+            />
+          </div>
+
+          <div className="inputTimerSet">
+            <h4>MM:</h4>
+            <input
+              type="range"
+              className="setTimerHours"
+              min={0}
+              max={59}
+              step={1}
+              onChange={(e) => {
+                setSelectedTimer({
+                  hoursSelected: selectedTimer?.hoursSelected
+                    ? selectedTimer.hoursSelected
+                    : 0,
+                  minutesSelected: parseFloat(e.target.value),
+                  secondsSelected: selectedTimer?.secondsSelected
+                    ? selectedTimer.secondsSelected
+                    : 0,
+                });
+              }}
+            />
+          </div>
+
+          <div className="inputTimerSet">
+            <h4>SS:</h4>
+            <input
+              type="range"
+              className="setTimerHours"
+              min={0}
+              max={59}
+              step={1}
+              onChange={(e) => {
+                setSelectedTimer({
+                  hoursSelected: selectedTimer?.hoursSelected
+                    ? selectedTimer.hoursSelected
+                    : 0,
+                  minutesSelected: selectedTimer?.minutesSelected
+                    ? selectedTimer.minutesSelected
+                    : 0,
+                  secondsSelected: parseFloat(e.target.value),
+                });
+              }}
+            />
+          </div>
         </section>
 
         <section className="timerActions">
