c4ed8391041edd02bc61e3f74c074b119253da29
Ryotruuser <ryonantruuser@gmail.com>
2025-12-26 10:30:18 -0300

    movido src de imagem da pasta public para assets para export com index.tsx

---
+++ src/assets/index.tsx
@@ -0,0 +1,25 @@
+import calcIcon from "./Calc.png";
+import cambioIcon from "./Cambio.png";
+import tasksIcon from "./Tasks.png";
+import timerIcon from "./Timer.png";
+import addTaskIcon from "./AddTask.png";
+import brFlag from "./brImage.png";
+import usFlag from "./UsaImage.png";
+import cnFlag from "./china.png";
+import euFlag from "./eu.png";
+import jpFlag from "./japan.png";
+import xIcon from "./x.png";
+
+export {
+  calcIcon,
+  cambioIcon,
+  tasksIcon,
+  timerIcon,
+  addTaskIcon,
+  brFlag,
+  usFlag,
+  cnFlag,
+  euFlag,
+  jpFlag,
+  xIcon,
+};
